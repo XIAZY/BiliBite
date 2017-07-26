68bc2737c869f199dbb2543b50373e0c575fadd3
Xia Zhongyang <i@mechanus.io>
2017-07-26 00:30:31 -0400

    Initial Commit

---
+++ BiliBite/AppDelegate.swift
@@ -0,0 +1,120 @@
+//
+//  AppDelegate.swift
+//  BiliBite
+//
+
+//  Copyright (c) 2017年 Xia Zhongyang. All rights reserved.
+//
+
+import UIKit
+import TVMLKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+    
+    var window: UIWindow?
+    var appController: TVApplicationController?
+    
+    // tvBaseURL points to a server on your local machine. To create a local server for testing purposes, use the following command inside your project folder from the Terminal app: ruby -run -ehttpd . -p9001. See NSAppTransportSecurity for information on using a non-secure server.
+    static let tvBaseURL = "http://localhost:9001/"
+    static let tvBootURL = "\(AppDelegate.tvBaseURL)/application.js"
+    
+    // MARK: Javascript Execution Helper
+    
+    func executeRemoteMethod(_ methodName: String, completion: @escaping (Bool) -> Void) {
+        appController?.evaluate(inJavaScriptContext: { (context: JSContext) in
+            let appObject : JSValue = context.objectForKeyedSubscript("App")
+            
+            if appObject.hasProperty(methodName) {
+                appObject.invokeMethod(methodName, withArguments: [])
+            }
+            }, completion: completion)
+    }
+    
+    // MARK: UIApplicationDelegate
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        // Create the TVApplicationControllerContext for this application and set the properties that will be passed to the `App.onLaunch` function in JavaScript.
+        let appControllerContext = TVApplicationControllerContext()
+        
+        // The JavaScript URL is used to create the JavaScript context for your TVMLKit application. Although it is possible to separate your JavaScript into separate files, to help reduce the launch time of your application we recommend creating minified and compressed version of this resource. This will allow for the resource to be retrieved and UI presented to the user quickly.
+        if let javaScriptURL = URL(string: AppDelegate.tvBootURL) {
+            appControllerContext.javaScriptApplicationURL = javaScriptURL
+        }
+        
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.tvBaseURL as NSString
+        
+        if let launchOptions = launchOptions {
+            for (kind, value) in launchOptions {
+                appControllerContext.launchOptions[kind.rawValue] = value
+            }
+        }
+        
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and stop playback
+        executeRemoteMethod("onWillResignActive", completion: { (success: Bool) in
+            // ...
+        })
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        executeRemoteMethod("onDidEnterBackground", completion: { (success: Bool) in
+            // ...
+        })
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        executeRemoteMethod("onWillEnterForeground", completion: { (success: Bool) in
+            // ...
+        })
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        executeRemoteMethod("onDidBecomeActive", completion: { (success: Bool) in
+            // ...
+        })
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        executeRemoteMethod("onWillTerminate", completion: { (success: Bool) in
+            // ...
+        })
+    }
+    
+    // MARK: TVApplicationControllerDelegate
+    
+    func appController(_ appController: TVApplicationController, didFinishLaunching options: [String: Any]?) {
+        print("\(#function) invoked with options: \(options ?? [:])")
+    }
+    
+    func appController(_ appController: TVApplicationController, didFail error: Error) {
+        print("\(#function) invoked with error: \(error)")
+        
+        let title = "Error Launching Application"
+        let message = error.localizedDescription
+        let alertController = UIAlertController(title: title, message: message, preferredStyle:.alert )
+        
+        self.appController?.navigationController.present(alertController, animated: true, completion: {
+            // ...
+        })
+    }
+    
+    func appController(_ appController: TVApplicationController, didStop options: [String: Any]?) {
+        print("\(#function) invoked with options: \(options ?? [:])")
+    }
+}
+
---
+++ BiliBite/application.js
@@ -0,0 +1,77 @@
+//# sourceURL=application.js
+
+/*
+ application.js
+ BiliBite
+ 
+ Copyright (c) 2017年 Xia Zhongyang. All rights reserved.
+*/
+
+/*
+ * This file provides an example skeletal stub for the server-side implementation 
+ * of a TVML application.
+ *
+ * A javascript file such as this should be provided at the tvBootURL that is 
+ * configured in the AppDelegate of the TVML application. Note that  the various 
+ * javascript functions here are referenced by name in the AppDelegate. This skeletal 
+ * implementation shows the basic entry points that you will want to handle 
+ * application lifecycle events.
+ */
+
+/**
+ * @description The onLaunch callback is invoked after the application JavaScript 
+ * has been parsed into a JavaScript context. The handler is passed an object 
+ * that contains options passed in for launch. These options are defined in the
+ * swift or objective-c client code. Options can be used to communicate to
+ * your JavaScript code that data and as well as state information, like if the 
+ * the app is being launched in the background.
+ *
+ * The location attribute is automatically added to the object and represents 
+ * the URL that was used to retrieve the application JavaScript.
+ */
+App.onLaunch = function(options) {
+    var alert = createAlert("Hello World!", "Welcome to tvOS");
+    navigationDocument.pushDocument(alert);
+}
+
+
+App.onWillResignActive = function() {
+
+}
+
+App.onDidEnterBackground = function() {
+
+}
+
+App.onWillEnterForeground = function() {
+    
+}
+
+App.onDidBecomeActive = function() {
+    
+}
+
+App.onWillTerminate = function() {
+    
+}
+
+
+/**
+ * This convenience funnction returns an alert template, which can be used to present errors to the user.
+ */
+var createAlert = function(title, description) {
+
+    var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+        <document>
+          <alertTemplate>
+            <title>${title}</title>
+            <description>${description}</description>
+          </alertTemplate>
+        </document>`
+
+    var parser = new DOMParser();
+
+    var alertDoc = parser.parseFromString(alertString, "application/xml");
+
+    return alertDoc
+}
